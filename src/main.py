a5f3a4167e7bf85fa6db6fdaf83d95061219b582
abhip999 <abhishekiitr999@gmail.com>
2024-03-19 11:10:06 +0530

    Create  main.py

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from utility_functions import *
+
+
+def sample_function():
+    print("-----------------------------------------------------------------\n")
+    print("This is a sample function for aitomation")
+    add(3,5)
+    print("------------------------End of the module-----------------------------------------\n")
+    
+
+
+
+
+
+
+
